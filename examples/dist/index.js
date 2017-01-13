jakezatecky/react-dual-listbox

commit cccb705a8bf1af96ecfcb5aa523fe2643f6fa013

Conform to latest ESLint rules

src/js/DualListBox.js
@@ -21,8 +21,15 @@ class DualListBox extends React.Component {
 		).isRequired,
 		available: React.PropTypes.arrayOf(React.PropTypes.string),
 		selected: React.PropTypes.arrayOf(React.PropTypes.string),
-		onChange: React.PropTypes.func,
+		onChange: React.PropTypes.func.isRequired,
 		preserveSelectOrder: React.PropTypes.bool,
+	};
+
+	static defaultProps = {
+		name: null,
+		available: undefined,
+		selected: [],
+		preserveSelectOrder: null,
 	};
 
 	/**
@@ -56,7 +63,7 @@ class DualListBox extends React.Component {
 			selected = direction === 'right' ? this.makeOptionsSelected(options) : [];
 		} else {
 			selected = this.toggleSelected(
-				this.getSelectedOptions(select)
+				this.getSelectedOptions(select),
 			);
 		}
 
@@ -87,7 +94,7 @@ class DualListBox extends React.Component {
 			const selected = this.toggleSelected(
 				[...currentTarget.options]
 					.filter(option => option.selected)
-					.map(option => option.value)
+					.map(option => option.value),
 			);
 
 			this.props.onChange(selected);
@@ -124,8 +131,8 @@ class DualListBox extends React.Component {
 	 */
 	getSelectedOptions(element) {
 		return [...element.options]
-			.filter((option) => option.selected)
-			.map((option) => option.value);
+			.filter(option => option.selected)
+			.map(option => option.value);
 	}
 
 	/**
@@ -210,16 +217,14 @@ class DualListBox extends React.Component {
 	 */
 	filterAvailable(options) {
 		if (this.props.available !== undefined) {
-			return this.filterOptions(options, (option) =>
+			return this.filterOptions(options, option =>
 				this.props.available.indexOf(option.value) >= 0 &&
-				this.props.selected.indexOf(option.value) < 0
+				this.props.selected.indexOf(option.value) < 0,
 			);
 		}
 
 		// Show all un-selected options
-		return this.filterOptions(options, (option) =>
-			this.props.selected.indexOf(option.value) < 0
-		);
+		return this.filterOptions(options, option => this.props.selected.indexOf(option.value) < 0);
 	}
 
 	/**
@@ -235,9 +240,7 @@ class DualListBox extends React.Component {
 		}
 
 		// Order the selections by the default order
-		return this.filterOptions(options, (option) =>
-			this.props.selected.indexOf(option.value) >= 0
-		);
+		return this.filterOptions(options, option => this.props.selected.indexOf(option.value) >= 0);
 	}
 
 	/**
@@ -250,7 +253,7 @@ class DualListBox extends React.Component {
 	filterSelectedByOrder(options) {
 		const labelMap = this.getLabelMap(options);
 
-		return this.props.selected.map((selected) => ({
+		return this.props.selected.map(selected => ({
 			value: selected,
 			label: labelMap[selected],
 		}));
@@ -260,17 +263,17 @@ class DualListBox extends React.Component {
 	 * @returns {Array}
 	 */
 	renderOptions(options) {
-		return options.map((option, index) => {
+		return options.map((option) => {
 			if (option.options !== undefined) {
 				return (
-					<optgroup key={index} label={option.label}>
+					<optgroup key={option.label} label={option.label}>
 						{this.renderOptions(option.options)}
 					</optgroup>
 				);
 			}
 
 			return (
-				<option key={index} value={option.value}>
+				<option key={option.value} value={option.value}>
 					{option.label}
 				</option>
 			);

.eslintrc
@@ -3,6 +3,8 @@
 	"parser": "babel-eslint",
 	"rules": {
 		"indent": [2, "tab"],
+		"class-methods-use-this": 0,
+		"no-tabs": 0,
 		"react/jsx-indent" : [2, "tab"],
 		"react/jsx-indent-props" : [2, "tab"],
 		"react/jsx-filename-extension": 0

src/js/Action.js
@@ -7,6 +7,10 @@ class Action extends React.Component {
 		onClick: React.PropTypes.func.isRequired,
 	};
 
+	static defaultProps = {
+		isMoveAll: false,
+	};
+
 	/**
 	 * @param {string} direction
 	 *

commit 1d2b4dbab1361b9b07c5da9a073b2b3565923e12

Re-enable index-based keys for options
Reverses backwards-compatibility-breaking changes introduced in cccb705a8bf1af96ecfcb5aa523fe2643f6fa013.

examples/dist/index.js
@@ -21884,18 +21884,22 @@ return /******/ (function(modules) { // webpackBootstrap
 			value: function renderOptions(options) {
 				var _this7 = this;
 
-				return options.map(function (option) {
+				return options.map(function (option, index) {
+					// Intentionally use array index to allow React to jump to next selection
+					// https://github.com/jakezatecky/react-dual-listbox/issues/10
+					var key = 'key-' + index;
+
 					if (option.options !== undefined) {
 						return _react2.default.createElement(
 							'optgroup',
-							{ key: option.label, label: option.label },
+							{ key: key, label: option.label },
 							_this7.renderOptions(option.options)
 						);
 					}
 
 					return _react2.default.createElement(
 						'option',
-						{ key: option.value, value: option.value },
+						{ key: key, value: option.value },
 						option.label
 					);
 				});

src/js/DualListBox.js
@@ -268,17 +268,21 @@ class DualListBox extends React.Component {
 	 * @returns {Array}
 	 */
 	renderOptions(options) {
-		return options.map((option) => {
+		return options.map((option, index) => {
+			// Intentionally use array index to allow React to jump to next selection
+			// https://github.com/jakezatecky/react-dual-listbox/issues/10
+			const key = `key-${index}`;
+
 			if (option.options !== undefined) {
 				return (
-					<optgroup key={option.label} label={option.label}>
+					<optgroup key={key} label={option.label}>
 						{this.renderOptions(option.options)}
 					</optgroup>
 				);
 			}
 
 			return (
-				<option key={option.value} value={option.value}>
+				<option key={key} value={option.value}>
 					{option.label}
 				</option>
 			);
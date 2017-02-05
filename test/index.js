c07770358978cd3cd817efd34770543408b1ef4b
Jake Zatecky <jake.zatecky@gmail.com>
2017-02-05 18:46:41 -0500

    Better organize tests

--- test/index.js
+++ test/index.js
@@ -5,63 +5,66 @@ import { assert } from 'chai';
 import DualListBox from '../src/js/DualListBox';
 
 describe('<DualListBox />', () => {
-	it('should render the react-dual-listbox container', () => {
-		const wrapper = shallow(<DualListBox options={[{ label: 'Phobos', value: 'phobos' }]} onChange={() => {}} />);
+	describe('component', () => {
+		it('should render the react-dual-listbox container', () => {
+			const wrapper = shallow(<DualListBox options={[{ label: 'Phobos', value: 'phobos' }]} onChange={() => {}} />);
 
-		assert.isTrue(wrapper.find('.react-dual-listbox').exists());
+			assert.isTrue(wrapper.find('.react-dual-listbox').exists());
+		});
 	});
 
-	it('should render the supplied options', () => {
-		const wrapper = shallow(<DualListBox
-			options={[
-				{ label: 'Moon', value: 'luna' },
-				{ label: 'Phobos', value: 'phobos' },
-			]}
-			onChange={() => {}}
-		/>);
+	describe('props.children', () => {
+		it('should render the supplied options', () => {
+			const wrapper = shallow(<DualListBox
+				options={[
+					{ label: 'Moon', value: 'luna' },
+					{ label: 'Phobos', value: 'phobos' },
+				]}
+				onChange={() => {}}
+			/>);
 
-		assert.isTrue(wrapper.contains(<option value="luna">Moon</option>));
-		assert.isTrue(wrapper.contains(<option value="phobos">Phobos</option>));
+			assert.isTrue(wrapper.contains(<option value="luna">Moon</option>));
+			assert.isTrue(wrapper.contains(<option value="phobos">Phobos</option>));
+		});
+
+		it('should render optgroups and their children', () => {
+			const wrapper = shallow(<DualListBox
+				options={[
+					{
+						label: 'Mars',
+						options: [
+							{ value: 'phobos', label: 'Phobos' },
+							{ value: 'deimos', label: 'Deimos' },
+						],
+					},
+				]}
+				onChange={() => {}}
+			/>);
+
+			assert.isTrue(wrapper.contains((
+				<optgroup label="Mars">
+					<option value="phobos">Phobos</option>
+					<option value="deimos">Deimos</option>
+				</optgroup>
+			)));
+		});
 	});
 
-	it('should render optgroups and their children', () => {
-		const wrapper = shallow(<DualListBox
-			options={[
-				{
-					label: 'Mars',
-					options: [
-						{ value: 'phobos', label: 'Phobos' },
-						{ value: 'deimos', label: 'Deimos' },
-					],
-				},
-			]}
-			onChange={() => {}}
-		/>);
+	describe('props.selected', () => {
+		it('should render selected options', () => {
+			const wrapper = shallow(<DualListBox
+				options={[
+					{ label: 'Moon', value: 'luna' },
+					{ label: 'Phobos', value: 'phobos' },
+				]}
+				onChange={() => {}}
+				selected={['phobos']}
+			/>);
 
-		assert.isTrue(wrapper.contains((
-			<optgroup label="Mars">
+			assert.isTrue(wrapper.find('.rdl-selected').contains((
 				<option value="phobos">Phobos</option>
-				<option value="deimos">Deimos</option>
-			</optgroup>
-		)));
-	});
-
-	it('should render selected and non-selected options', () => {
-		const wrapper = shallow(<DualListBox
-			options={[
-				{ label: 'Moon', value: 'luna' },
-				{ label: 'Phobos', value: 'phobos' },
-			]}
-			onChange={() => {}}
-			selected={['phobos']}
-		/>);
-
-		assert.isTrue(wrapper.find('.rdl-available').contains((
-			<option value="luna">Moon</option>
-		)));
-		assert.isTrue(wrapper.find('.rdl-selected').contains((
-			<option value="phobos">Phobos</option>
-		)));
+			)));
+		});
 	});
 
 	describe('props.available', () => {

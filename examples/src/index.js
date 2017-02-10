17691f286652ca705a3cf34eb1d6cdb01341c22c
Jake Zatecky <jake.zatecky@gmail.com>
2017-02-09 19:06:50 -0500

    Add preserveSelectOrder example

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import BasicExample from './js/BasicExample';
+import PreserveSelectOrderExample from './js/PreserveSelectOrderExample';
 import OptGroupExample from './js/OptGroupExample';
 import RestrictAvailable from './js/RestrictAvailableExample';
 
 ReactDOM.render(<BasicExample />, document.getElementById('basic-example'));
 ReactDOM.render(<OptGroupExample />, document.getElementById('optgroup-example'));
+ReactDOM.render(<PreserveSelectOrderExample />, document.getElementById('preserve-select-order-example'));
 ReactDOM.render(<RestrictAvailable />, document.getElementById('restrict-available-example'));
---
+++ examples/src/js/PreserveSelectOrderExample.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import DualListBox from '../../../src/js/DualListBox';
+
+const options = [
+	{
+		label: 'Earth',
+		options: [
+			{ value: 'luna', label: 'Moon' },
+		],
+	},
+	{
+		label: 'Mars',
+		options: [
+			{ value: 'phobos', label: 'Phobos' },
+			{ value: 'deimos', label: 'Deimos' },
+		],
+	},
+	{
+		label: 'Jupiter (Galilean moons)',
+		options: [
+			{ value: 'io', label: 'Io' },
+			{ value: 'europa', label: 'Europa' },
+			{ value: 'ganymede', label: 'Ganymede' },
+			{ value: 'callisto', label: 'Callisto' },
+		],
+	},
+];
+
+class PreserveSelectOrderExample extends React.Component {
+	constructor() {
+		super();
+
+		this.state = { selected: [] };
+
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange(selected) {
+		this.setState({ selected });
+	}
+
+	render() {
+		return (
+			<DualListBox
+				name="moons"
+				options={options}
+				preserveSelectOrder
+				selected={this.state.selected}
+				onChange={this.onChange}
+			/>
+		);
+	}
+}
+export default PreserveSelectOrderExample;

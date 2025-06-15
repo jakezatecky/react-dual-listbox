91edfa0714875c0e4a5e4121f5e2c1a11d505274
Jake Zatecky <jake.zatecky@gmail.com>
2025-06-15 19:10:55 -0400

    Fix prop type misalignment with language object
    Resolves #297.

--- src/js/shapes/languageShape.js
+++ src/js/shapes/languageShape.js
@@ -2,17 +2,21 @@ import PropTypes from 'prop-types';
 
 export default PropTypes.shape({
     availableFilterHeader: PropTypes.string,
+    availableFilterPlaceholder: PropTypes.string,
     availableHeader: PropTypes.string,
-    filterPlaceholder: PropTypes.string,
+    hiddenInputLabel: PropTypes.string,
     moveToAvailable: PropTypes.string,
     moveAllToAvailable: PropTypes.string,
     moveToSelected: PropTypes.string,
     moveAllToSelected: PropTypes.string,
+    moveBottom: PropTypes.string,
     moveDown: PropTypes.string,
+    moveTop: PropTypes.string,
     moveUp: PropTypes.string,
     noAvailableOptions: PropTypes.string,
     noSelectedOptions: PropTypes.string,
     requiredError: PropTypes.string,
     selectedFilterHeader: PropTypes.string,
+    selectedFilterPlaceholder: PropTypes.string,
     selectedHeader: PropTypes.string,
 });
